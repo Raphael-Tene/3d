d7e19eb7becfd8f757277eb0e8863bdb319072f5
tango <teneraphael@gmail.com>
2023-04-05 18:13:50 +0000

    learned about Azimuth and polar angles

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import { angleToRads } from "../../../utils/angle";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
 export default function Three() {
-  useFrame((state) => {});
+  const orbitControlsRef = useRef(null);
+
+  useFrame((state) => {
+    if (!!orbitControlsRef.current) {
+      const { x, y } = state.mouse;
+      // @ts-ignore
+      orbitControlsRef.current.setAzimuthalAngle(-x * angleToRads(45));
+      orbitControlsRef.current.setPolarAngle((y + 0.5) * angleToRads(90 - 30));
+      // @ts-ignore
+      orbitControlsRef.current.update();
+    }
+  });
 
   return (
     <>
-      <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-      <OrbitControls />
+      <PerspectiveCamera makeDefault position={[0, 1, 10]} />
+      <OrbitControls
+        ref={orbitControlsRef}
+        minPolarAngle={angleToRads(60)}
+        maxPolarAngle={angleToRads(80)}
+        enableZoom={false}
+      />
       {/* creating a ball component */}
       <mesh position={[0, 1, 0]}>
         <sphereGeometry args={[1, 32, 32]} />
